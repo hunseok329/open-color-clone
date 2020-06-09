16523f2bba16e71ad12f1ff2deea953ee59dbd62
hunseok329 <kha5064@gmail.com>
2020-06-09 22:54:34 +0900

    create color-sample

---
+++ src/component/colorChips/color-sample.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function ColorSample() {
+  return (
+    <div className="shortcut-wrap">
+      <ul className="shortcut-menus">
+        <li>
+          <a className="shortcut"></a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default ColorSample;
--- src/component/title/color-title.js
+++ src/component/title/color-title.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./color-title.css";
 
 function colorTitle() {
   return (
