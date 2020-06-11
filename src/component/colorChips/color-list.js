082f1a81f2cc11e126555f436a4996b2b59bc3e1
hunseok329 <kha5064@gmail.com>
2020-06-11 21:10:58 +0900

    color-list css to change style-component

--- src/component/colorChips/color-list.js
+++ src/component/colorChips/color-list.js
@@ -1,35 +1,65 @@
 import React from "react";
+import styled from "styled-components";
 import ColorGroup from "./color-group";
+import "./color-list.css";
 
 //Sum all the values and return "ColorList"
 function ColorList(props) {
   return (
-    <div className="row">
-      <div className="shortcut-wrap">
-        <ul className="shortcut-menus">{props.allColor.map(renderSample)}</ul>
+    <div className="container">
+      <div className="row">
+        <div className="shortcut-wrap">
+          <ul className="shortcut-menus">{props.allColor.map(renderSample)}</ul>
+        </div>
+        {props.allColor.map(renderColor)}
       </div>
-      {props.allColor.map(renderColor)}
     </div>
   );
 }
 
 //Take a "name" from "AllColor" and give it to ColorSample
-function renderSample(sample) {
-  return <ColorSample sampleName={sample.name}></ColorSample>;
+function renderSample(props) {
+  return (
+    <ColorSample
+      sampleName={props.name}
+      color={props.chips.chip7}
+    ></ColorSample>
+  );
 }
+
 //Give "AllColor" to ColorGroup
-function renderColor(color) {
-  return <ColorGroup name={color.name} colorsChip={color.chips}></ColorGroup>;
+function renderColor(props) {
+  return <ColorGroup name={props.name} colorsChip={props.chips}></ColorGroup>;
 }
+
 //Make each shortcut "<li>"
 function ColorSample(props) {
-  console.log(props);
   var aClass = "shortcut " + props.sampleName;
+  var hre = "#" + props.sampleName;
   return (
     <li>
-      <a className={aClass}>{props.sampleName}</a>
+      <a className={aClass} href={hre}>
+        <Span color={props.color}></Span>
+        <Name hcolor={props.color}>{props.sampleName}</Name>
+      </a>
     </li>
   );
 }
 
+const Span = styled.span`
+  display: inline-block;
+  margin-top: -2px;
+  margin-right: 10px;
+  width: 14px;
+  height: 14px;
+  border-radius: 4px;
+  vertical-align: middle;
+  background-color: ${(props) => props.color};
+`;
+
+const Name = styled.span`
+  &:hover {
+    color: ${(props) => props.hcolor};
+  }
+`;
 export default ColorList;
